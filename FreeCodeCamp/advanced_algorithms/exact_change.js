d4b1b60449b724cf7d6f2428110e8e3f1836988e
rebelBeard <rfm144@gmail.com>
2016-12-29 21:00:26 -0500

    FCC Advanced Algorithms Exact Change
    Solution for FCC Advanced Algorithms Challenge: Exact Change

--- FreeCodeCamp/advanced_algorithms/exact_change.js
+++ FreeCodeCamp/advanced_algorithms/exact_change.js
@@ -13,32 +13,57 @@ Otherwise, return change in coin and bills, sorted in highest to lowest order.
 */
 
 function checkCashRegister(price, cash, cid) {
-  var change;
 
   // function totals all cash in the drawer and returns it
   function checkCashInDrawer() {
-    var totalCashInDrawer = 0;
     cid.forEach(function(item){
       totalCashInDrawer += item[1];
     });
     return Number(totalCashInDrawer.toFixed(2));
   }
 
-  var totalCashInDrawer = checkCashInDrawer();
-  change = cash - price;
+  var totalCashInDrawer = 0;
+  var change = cash - price;
+  var denominations = [
+    {name: "ONE HUNDRED", value: 100},
+    {name: "TWENTY", value: 20},
+    {name: "TEN", value: 10},
+    {name: "FIVE", value: 5},
+    {name: "ONE", value: 1},
+    {name: "QUARTER", value: 0.25},
+    {name: "DIME", value: 0.10},
+    {name: "NICKEL", value: 0.05},
+    {name: "PENNY", value: 0.01}
+  ];
+  totalCashInDrawer = checkCashInDrawer();
+  cid = cid.reverse();
+
 
 
   // check if total cash in drawer is equal to the change amount
-  if (totalCashInDrawer === change) {
-    return "closed";
-  }
-
-  // check if total cash in drawer is less than change amount
   if (totalCashInDrawer < change) {
     return "Insufficient Funds";
+    // check if total cash in drawer is less than change amount
+  } else if (totalCashInDrawer === change) {
+    return "Closed";
+  } else {
+    var changeGiven = denominations.reduce(function(acc, next, i) {
+      if (change > next.value) {
+        var currentValue = 0.0;
+        while (change >= next.value && cid[i][1] >= next.value) {
+          currentValue += next.value;
+          change -= next.value;
+          change = Math.round(change * 100) / 100;
+          cid[i][1] -= next.value;
+        }
+        acc.push([next.name, currentValue]);
+        return acc;
+      } else {
+        return acc;
+      }
+    }, []);
+    return changeGiven.length > 0 && change === 0 ? changeGiven: "Insufficient Funds";
   }
-
-
 }
 
 // Example cash-in-drawer array:
@@ -52,6 +77,21 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])); // should return an array
+// Test Cases
 
-console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // "closed"
+console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
+// should return an array
+
+console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
+//[["QUARTER", 0.50]]
+
+console.log(checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
+//[["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]]
+
+
+
+console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+// "closed"
+
+console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+// "Insufficient Funds"
